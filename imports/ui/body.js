c707cf72fe3d92b8324721476018e6113c8eacec
Yev <yevtarler@gmail.com>
2016-06-20 23:33:27 +0300

    added methods - db, db comunication w validation

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,41 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating' ;
 
 import './body.html'
 
 Template.body.helpers ( {
+<<<<<<< Updated upstream
     tasks: [
       { text: 'This is task 1' },
       { text: 'This is task 2' },
       { text: 'This is task 3' },
     ],
+=======
+    tasks() {
+      const instance = Template.instance();
+      if (instance.state.get('hideCompleted')) {
+        return Tasks.find({checked: { $ne: true }}, {sort:{createdAt: -1}});
+      }
+      return Tasks.find({}, {sort: { createdAt: -1}});
+    },
+    incompleteCount() {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Meteor.call('tasks.insert', text);
+
+    target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted',event.target.checked)
+  }
+>>>>>>> Stashed changes
 });
---
+++ imports/ui/task.js
@@ -0,0 +1,15 @@
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+import { Tasks } from '../api/tasks.js';
+
+import  './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    Meteor.call('tasks.setChecked', this._id, !this.checked)
+  },
+  'click .delete'() {
+    Meteor.call('tasks.remove', this._id);
+  }
+});
