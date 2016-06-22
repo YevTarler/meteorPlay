24e283660eaf063e245ebe658e3bc70be9a5a1b2
Yev <yevtarler@gmail.com>
2016-06-22 23:02:56 +0300

    final commit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,41 +1,49 @@
+
 import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating' ;
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
-import './body.html'
+import { Tasks } from '../api/tasks.js';
 
-Template.body.helpers ( {
-<<<<<<< Updated upstream
-    tasks: [
-      { text: 'This is task 1' },
-      { text: 'This is task 2' },
-      { text: 'This is task 3' },
-    ],
-=======
-    tasks() {
-      const instance = Template.instance();
-      if (instance.state.get('hideCompleted')) {
-        return Tasks.find({checked: { $ne: true }}, {sort:{createdAt: -1}});
-      }
-      return Tasks.find({}, {sort: { createdAt: -1}});
-    },
-    incompleteCount() {
-      return Tasks.find({checked: {$ne: true}}).count();
+import './task.js';
+import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
+});
+
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
     }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
 
 Template.body.events({
   'submit .new-task'(event) {
+    // Prevent default browser form submit
     event.preventDefault();
 
+    // Get value from form element
     const target = event.target;
     const text = target.text.value;
 
+    // Insert a task into the collection
     Meteor.call('tasks.insert', text);
 
+    // Clear form
     target.text.value = '';
   },
   'change .hide-completed input'(event, instance) {
-    instance.state.set('hideCompleted',event.target.checked)
-  }
->>>>>>> Stashed changes
+    instance.state.set('hideCompleted', event.target.checked);
+  },
 });
